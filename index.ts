7f34669fdb928bf9d1f3cb12aca4dcccc6a02f38
Cass Barnard <30302915+cass-codes@users.noreply.github.com>
2025-09-04 21:28:23 -0400

    init and basic command line parsing

---
+++ index.ts
@@ -0,0 +1,14 @@
+// Step 1. Get the file
+const testFileArgument = process.argv[2];
+
+// Step 2. Parse the file into Floodplanes and Parcels
+
+// Step 3. Initialize the Floodplane Map
+
+// Step 4. Gather the Parcel Data:
+// Step 4.1. For each Parcel, gather all Floodplanes in the Parcel's area
+// Step 4.2. Determine the highest risk Floodplane of the Pacel's floodplanes (if any)
+
+// Step 5. Print ParcelID with Riskiest Floodplane (omit if no flood plane)
+
+console.log('testFileArgument', testFileArgument);
